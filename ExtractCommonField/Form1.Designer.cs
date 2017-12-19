0df4d2f6de3f968fc348ab47fcd84af32644240c
孙文权 <604164227@qq.com>
2017-12-19 19:55:31 +0800

    添加项目文件。

---
+++ ExtractCommonField/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ExtractCommonField
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.btnSelFile = new System.Windows.Forms.Button();
+            this.btnSelDirectory = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(12, 12);
+            this.txtClassName.Multiline = true;
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(260, 42);
+            this.txtClassName.TabIndex = 0;
+            this.txtClassName.Text = "CqBankXdbAssessmentResult1,CqBankXdbAssessmentResult2,CqBankXdbAssessmentResult3," +
+    "CqBankXdbAssessmentResult4";
+            // 
+            // btnSelFile
+            // 
+            this.btnSelFile.Location = new System.Drawing.Point(12, 60);
+            this.btnSelFile.Name = "btnSelFile";
+            this.btnSelFile.Size = new System.Drawing.Size(120, 23);
+            this.btnSelFile.TabIndex = 1;
+            this.btnSelFile.Text = "选择文件";
+            this.btnSelFile.UseVisualStyleBackColor = true;
+            this.btnSelFile.Click += new System.EventHandler(this.btnSelFile_Click);
+            // 
+            // btnSelDirectory
+            // 
+            this.btnSelDirectory.Location = new System.Drawing.Point(152, 60);
+            this.btnSelDirectory.Name = "btnSelDirectory";
+            this.btnSelDirectory.Size = new System.Drawing.Size(120, 23);
+            this.btnSelDirectory.TabIndex = 2;
+            this.btnSelDirectory.Text = "选择目录";
+            this.btnSelDirectory.UseVisualStyleBackColor = true;
+            this.btnSelDirectory.Click += new System.EventHandler(this.btnSelDirectory_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(13, 90);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(259, 159);
+            this.txtResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnSelDirectory);
+            this.Controls.Add(this.btnSelFile);
+            this.Controls.Add(this.txtClassName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "提取公共字段";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtClassName;
+        private System.Windows.Forms.Button btnSelFile;
+        private System.Windows.Forms.Button btnSelDirectory;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ ExtractCommonField/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace ExtractCommonField
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //var classNameList = new List<string> { "CqBankXdbAssessmentResult1", "CqBankXdbAssessmentResult2", "CqBankXdbAssessmentResult3", "CqBankXdbAssessmentResult4" };
+            //var assembly = Assembly.LoadFrom(@"C:\Work\DotNetTeamGit\Caad.Viss\Source\Common\Caad.Api\bin\Debug\Caad.Api.dll");
+            //var types = assembly.GetTypes().Where(c => c.IsClass && classNameList.Contains(c.Name)).ToList();
+            //var minCount = int.MaxValue;
+            //Type minType = null;
+            //foreach (var type in types)
+            //{
+            //    var count = type.GetProperties().Length;
+            //    if (count >= minCount)
+            //    {
+            //        continue;
+            //    }
+            //    minCount = count;
+            //    minType = type;
+            //}
+            //if (minType == null)
+            //{
+            //    return;
+            //}
+            //var propertyNameList = new List<string>();
+            //foreach (var propertyInfo in minType.GetProperties())
+            //{
+            //    if (types.Count(c => c.GetProperties().Any(s => s.Name == propertyInfo.Name && s.PropertyType == propertyInfo.PropertyType)) != types.Count)
+            //    {
+            //        continue; ;
+            //    }
+            //    propertyNameList.Add(propertyInfo.Name);
+            //}
+            //MessageBox.Show(string.Join(",", propertyNameList));
+            ////types.FirstOrDefault(c=>c.GetTypeInfo().GetProperties().m)
+            ////var typeMin = types.Max(c => c.GetTypeInfo().GetProperties().Length);
+
+        }
+
+        private string[] GetClassNameList()
+        {
+            return txtClassName.Text.Split(',');
+        }
+
+        private string Search(string[] classNameList, List<Assembly> assemblyList)
+        {
+            //var classNameList = new List<string> { "CqBankXdbAssessmentResult1", "CqBankXdbAssessmentResult2", "CqBankXdbAssessmentResult3", "CqBankXdbAssessmentResult4" };
+            var assembly = Assembly.LoadFrom(@"C:\Work\DotNetTeamGit\Caad.Viss\Source\Common\Caad.Api\bin\Debug\Caad.Api.dll");
+            var types = new List<Type>();
+            foreach (var assembly1 in assemblyList)
+            {
+                types.AddRange(assembly1.GetTypes().Where(s => s.IsClass && classNameList.Contains(s.Name)).ToList());
+            }
+            var minCount = int.MaxValue;
+            Type minType = null;
+            foreach (var type in types)
+            {
+                var count = type.GetProperties().Length;
+                if (count >= minCount)
+                {
+                    continue;
+                }
+                minCount = count;
+                minType = type;
+            }
+            if (minType == null)
+            {
+                return string.Empty;
+            }
+            var propertyNameList = new List<string>();
+            foreach (var propertyInfo in minType.GetProperties())
+            {
+                if (types.Count(c => c.GetProperties().Any(s => s.Name == propertyInfo.Name && s.PropertyType == propertyInfo.PropertyType)) != types.Count)
+                {
+                    continue;
+                }
+                propertyNameList.Add(propertyInfo.Name);
+            }
+            return string.Join(",", propertyNameList);
+        }
+
+        private void btnSelFile_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                Filter = @"dll|*.dll;",
+                Multiselect = true
+            };
+            if (ofd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            var assemblyList = ofd.FileNames.Select(Assembly.LoadFrom).ToList();
+            Search(GetClassNameList(), assemblyList);
+        }
+
+        private void btnSelDirectory_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            var assemblyList = Directory.GetFiles(fbd.SelectedPath, "*.dll", SearchOption.AllDirectories)
+                .Select(Assembly.LoadFrom).ToList();
+            Search(GetClassNameList(), assemblyList);
+        }
+    }
+}
